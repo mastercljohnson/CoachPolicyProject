35a7d28220e707fea6500211eed957608bae623c
Christopher Johnson <mastercljohnson@gmail.com>
2025-08-24 15:22:48 -0400

    Add adj matrix encoding for simple gnn

--- gnn/gnn.py
+++ gnn/gnn.py
@@ -1,8 +1,11 @@
 from torch import nn
 
 class GNN(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, num_vertices, hidden_dim, **kwargs):
         super().__init__()
+
+        self.adj_mat_ff = nn.Linear(num_vertices, hidden_dim)
     
     def forward(self, x, edge_index):
+        adj_enc = self.adj_mat_ff(x)
         raise NotImplementedError
