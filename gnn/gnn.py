1c8adf9175399eb9ea9db1e910ee8186065686dc
Christopher Johnson <mastercljohnson@gmail.com>
2025-09-15 16:13:50 -0400

    update so can use on other laptop

--- gnn/gnn.py
+++ gnn/gnn.py
@@ -1,5 +1,12 @@
 from torch import nn
 
+#  1. Use GNN for task allocation
+#  2. Create Q network for graph result 
+#  Backprop q loss with actual reward from env.
+
+#  3. Create policies for subtasks that take partial observ and zero pad full
+#  4. Create Q networks for each agent, with hypernetworks to generate weights >=0 weights
+
 class GNN(nn.Module):
     def __init__(self, num_vertices, hidden_dim, **kwargs):
         super().__init__()
