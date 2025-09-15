91eabe8005f73385b75e95ddda54e4d226fbe37a
Christopher Johnson <mastercljohnson@gmail.com>
2025-09-15 17:47:27 -0400

    adding mask for adjacency matrix

---
+++ adj_mask/adj_experiment_run.py
@@ -0,0 +1,53 @@
+from pettingzoo.sisl import multiwalker_v9
+from dtcg import DTCG
+
+# observation is a dictionary with keys as agent names and values as their respective observations
+# action is a dictionary with keys as agent names and values as their respective actions
+# Not sure what infos is
+
+env = multiwalker_v9.parallel_env(render_mode="human")
+observations, infos = env.reset()
+
+
+algo = DTCG(env.agents, observation_spaces=env.observation_spaces, action_spaces=env.action_spaces)
+
+rewards = None
+
+total_steps = 200
+
+backprop_steps = 10
+
+env_step = 0
+cumulative_rewards = 0
+termination_signal = False
+while env.agents and env_step < total_steps:
+    # this is where you would insert your policy
+    total_rewards = sum(rewards.values()) if rewards else 0 # place holder for total rewards 
+    cumulative_rewards += total_rewards
+
+    actions = algo.act(observations, cumulative_rewards, env_step)  # Get actions from the DTCG algorithm
+    actions = {agent: env.action_space(agent).sample() for agent in env.agents} # this is a dictionary
+    observations, rewards, terminations, truncations, infos = env.step(actions)
+    env_step += 1
+    termination_signal = any(terminations.values()) or any(truncations.values())
+
+    # Uhhhh, here what happens for the online case?
+    #  I need something that generates good trajectories
+    if env_step % backprop_steps == 0:
+        # Backpropagate the rewards through the algorithm
+        continue
+
+    if termination_signal:
+        print("Terminates or truncates detected.")
+        #  Load the terminal state, I dont think we need to do this
+        # algo.load_state_buffer(observations)
+        # Calculate returns to go
+        algo.load_returns_to_go(cumulative_rewards, env_step)
+
+        # Reset the environment
+        observations, infos = env.reset()
+        termination_signal = False
+        cumulative_rewards = 0
+        env_step = 0
+
+env.close()
---
+++ adj_mask/adj_mask.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+
+class AdjMask(nn.Module):
+    def __init__(self, hidden_dim, adj_mask=None, **kwargs):
+        super().__init__()
+        self.c_attn = nn.Linear(hidden_dim, 3 * hidden_dim, bias=False)
+        self.adj_mask = adj_mask
+        # output projection
+        self.c_proj = nn.Linear(hidden_dim, hidden_dim, bias=False)
+        self.hidden_dim = hidden_dim
+        self.n_head = 1
+        self.dropout = 0.1
+        self.resid_dropout = nn.Dropout(self.dropout)
+    
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k, v  = self.c_attn(x).split(self.hidden_dim, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=self.adj_mask, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
+        
+    
+
+if __name__ == "__main__":
+    adj_mask = torch.tensor([[1,1,0],[1,1,1],[0,1,1]],dtype=torch.bool)  # Example adjacency mask for 3 nodes
+    model = AdjMask(hidden_dim=32)
+    print(model)
+    x = torch.randn(3, 3, 32)  # (batch_size, seq_length, hidden_dim)
+    output = model(x)
+    print(output.shape)  # should be (4, 3, 32)
