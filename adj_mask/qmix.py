4724143f9e0a82e7e645af1ac6ad0e2e255792ca
Christopher Johnson <mastercljohnson@gmail.com>
2025-09-17 12:15:18 -0400

    skeleton for mixing network hypernetwork param generation

--- adj_mask/qmix.py
+++ adj_mask/qmix.py
@@ -1,19 +1,33 @@
 import torch
 from torch import nn
 class QMix(nn.Module):
-    def __init__(self, num_agents, state_space, action_space, **kwargs):
+    def __init__(self, num_agents, state_space, action_space, hidden_dim, **kwargs):
         super().__init__()
         self.num_agents = num_agents
         for i in range(num_agents):
             setattr(self, f"agent_{i}_policy_network", nn.Linear(state_space, action_space))
+            setattr(self, f"agent_{i}_q_network", nn.Linear(state_space + action_space, 1))
+        self.hyper_network_1 = nn.Linear(state_space, hidden_dim*num_agents) # W1 weights
+        self.hyper_network_2 = nn.Linear(state_space, hidden_dim*num_agents) # Layernorm+ residual
+        self.hyper_network_3 = nn.Linear(state_space, 1) # W2 weights
+        self.hyper_network_4 = nn.Linear(state_space, 1) # layernorm + residual 1
+        self.hyper_network_5 = nn.Linear(state_space, 1) # layernorm + residual 2
     
     def forward(self, states):
         actions = []
+        q_values = []
         for i in range(self.num_agents):
             agent_policy_network = getattr(self, f"agent_{i}_policy_network")
             agent_action = agent_policy_network(states[:, i, :])
             actions.append(agent_action)
+            agent_q_network = getattr(self, f"agent_{i}_q_network")
+            agent_q_input = torch.cat([states[:, i, :], agent_action], dim=-1)
+            agent_q_value = agent_q_network(agent_q_input)
+            q_values.append(agent_q_value)
         actions = torch.stack(actions, dim=1)
+        q_values = torch.stack(q_values, dim=-1)
+
+
         return actions
     
 if __name__ == "__main__":
