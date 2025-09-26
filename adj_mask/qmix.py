3a6f838489210c460b0a12ef32cd18e4ae138458
Christopher Johnson <mastercljohnson@gmail.com>
2025-09-26 14:03:19 -0400

    Fixing dimensions for hyper network

--- adj_mask/qmix.py
+++ adj_mask/qmix.py
@@ -5,12 +5,13 @@ class QMix(nn.Module):
         super().__init__()
         self.num_agents = len(agents)
         self.state_space = sum([state_space[agent].shape[0] for agent in agents])
+        self.hidden_dim = hidden_dim
         for i, agent in enumerate(agents):
             # print(state_space[agent].shape[0], action_space[agent].shape[0])
             setattr(self, f"agent_{i}_policy_network", nn.Linear(state_space[agent].shape[0], action_space[agent].shape[0]))
             setattr(self, f"agent_{i}_q_network", nn.Linear(state_space[agent].shape[0] + action_space[agent].shape[0], 1))
         self.hyper_network_weight_1 = nn.Linear(self.state_space, hidden_dim*self.num_agents) # W1 weights
-        self.hyper_network_bias_1 = nn.Linear(self.state_space, self.num_agents) # bias
+        self.hyper_network_bias_1 = nn.Linear(self.state_space, hidden_dim) # bias
         self.hyper_network_weight_2 = nn.Linear(self.state_space, hidden_dim*self.num_agents) # W2 weights
         self.hyper_network_bias_2_1 = nn.Linear(self.state_space, hidden_dim) # bias2
         self.hyper_network_bias_2_2 = nn.Linear(hidden_dim, self.num_agents) # bias2
@@ -21,19 +22,28 @@ class QMix(nn.Module):
         for i in range(self.num_agents):
             agent_policy_network = getattr(self, f"agent_{i}_policy_network")
             agent_action = agent_policy_network(states[:, i, :])
+            print("agent_action", agent_action.shape)
             actions.append(agent_action)
             agent_q_network = getattr(self, f"agent_{i}_q_network")
             agent_q_input = torch.cat([states[:, i, :], agent_action], dim=-1)
+            print("agent_q_input", agent_q_input.shape)
             agent_q_value = agent_q_network(agent_q_input)
+            print("agent_q_value", agent_q_value.shape)
             q_values.append(agent_q_value)
         actions = torch.stack(actions, dim=1)
-        q_values = torch.stack(q_values, dim=-1)
+        print(q_values)
+        q_values = torch.cat(q_values, dim=-1)
+        print("q_values", q_values.shape)
 
-        hyper_w1 = torch.abs(self.hyper_network_1(states).view(-1, self.num_agents, -1))  # (batch_size, num_agents, hidden_dim)
-        hyper_b1 = self.hyper_network_2(states).view(-1, 1, -1)  # (batch_size, 1, hidden_dim)
-        hidden = torch.relu(torch.bmm(q_values.unsqueeze(1), hyper_w1) + hyper_b1)
-        hyper_w2 = torch.abs(self.hyper_network_3(states).view(-1, hidden.size(-1), 1))  # (batch_size, hidden_dim, 1)
-        hyper_b2 = self.hyper_network_4(states).view(-1, 1, 1)  # (batch_size, 1, 1)
+
+        hyper_state = torch.cat([states[:, i, :] for i in range(self.num_agents)], dim=-1)
+        hyper_w1 = torch.abs(self.hyper_network_weight_1(hyper_state).view(-1, self.num_agents, self.hidden_dim))  # (batch_size, self.num_agents, hidden_dim)
+        hyper_b1 = self.hyper_network_bias_1(hyper_state).view(-1, 1, self.hidden_dim)  # (batch_size, 1, hidden_dim)
+        print(q_values.shape, hyper_w1.shape, hyper_b1.shape)
+        hidden = torch.relu(torch.bmm(q_values, hyper_w1) + hyper_b1)
+        hyper_w2 = torch.abs(self.hyper_network_weight_2(hyper_state).view(-1, hidden.size(-1), 1))  # (batch_size, hidden_dim, 1)
+        hyper_b2 = torch.nn.relu(self.hyper_network_bias_2_1(hyper_state).view(-1, 1, 1))  # (batch_size, 1, 1)
+        hyper_b2 = self.hyper_network_bias_2_2(hyper_b2).view(-1, 1, 1)  # (batch_size, 1, 1)
         q_total = torch.bmm(hidden, hyper_w2) + hyper_b2  # (batch_size, 1, 1)
         q_total = q_total.squeeze(-1).squeeze(-1)  # (batch_size,)
 
