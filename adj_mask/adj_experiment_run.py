51696d84e261338ef1f0b198ecfca78dab9a3572
Christopher Johnson <mastercljohnson@gmail.com>
2025-10-02 12:09:19 -0400

    add optimizer and detect issues with loss

--- adj_mask/adj_experiment_run.py
+++ adj_mask/adj_experiment_run.py
@@ -15,27 +15,41 @@ algo = AdjFrame(env.agents,31,31, env.observation_spaces, env.action_spaces)
 
 rewards = None
 
-total_steps = 200
+total_steps = 100000
 
 backprop_steps = 10
 
 env_step = 0
 cumulative_rewards = 0
 termination_signal = False
+
+lr = 1e-3 # learning_rate / 10 usually
+beta1 = 0.9 # momentum term for Adam optimizer
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+weight_decay = 1e-2 # L2 regularization term
+
+optimizer = torch.optim.AdamW(algo.parameters(), lr=lr, betas=(beta1, beta2), weight_decay=weight_decay)
+
 while env.agents and env_step < total_steps:
     # this is where you would insert your policy
     total_rewards = sum(rewards.values()) if rewards else 0 # place holder for total rewards 
     cumulative_rewards += total_rewards
 
     actions, q_total = algo.forward(observations)  # Forward pass
-    print("Actions from the model:", actions)
+    # print("Actions from the model:", actions)
     
     # actions = algo.act(observations, cumulative_rewards, env_step)  # Get actions from the DTCG algorithm
     # actions = {agent: env.action_space(agent).sample() for agent in env.agents} # this is a dictionary
     observations, rewards, terminations, truncations, infos = env.step(actions)
     env_step += 1
 
+    
     loss = algo.loss(q_total, torch.tensor([cumulative_rewards], dtype=torch.float32))
+    print("Loss:", loss.item())
+    algo.train()
+    loss.backward()
+    optimizer.zero_grad()
+    
 
     termination_signal = any(terminations.values()) or any(truncations.values())
 
@@ -53,6 +67,6 @@ while env.agents and env_step < total_steps:
         observations, infos = env.reset()
         termination_signal = False
         cumulative_rewards = 0
-        env_step = 0
+        # env_step = 0
 
 env.close()
