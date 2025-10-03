724786ad6cbe39cfa70bb691f08afaa77fb4f909
Christopher Johnson <mastercljohnson@gmail.com>
2025-10-03 13:43:53 -0400

    Experiment with regular attention mask

--- adj_mask/adj_experiment_run.py
+++ adj_mask/adj_experiment_run.py
@@ -1,13 +1,14 @@
 from pettingzoo.sisl import multiwalker_v9
 from adj_framework import AdjFrame
 import torch
+import numpy as np
 
 # observation is a dictionary with keys as agent names and values as their respective observations
 # action is a dictionary with keys as agent names and values as their respective actions
 # Not sure what infos is
 
 env = multiwalker_v9.parallel_env(render_mode="human",terminate_on_fall=True)
-observations, infos = env.reset()
+observations, infos = env.reset(seed=0)
 
 
 # hidden dim right now needs to match observation dimensions
@@ -15,7 +16,7 @@ algo = AdjFrame(env.agents,31,31, env.observation_spaces, env.action_spaces)
 
 rewards = None
 
-total_steps = 1000
+total_steps = 10000000
 
 backprop_steps = 10
 
@@ -30,35 +31,45 @@ weight_decay = 1e-2 # L2 regularization term
 
 optimizer = torch.optim.AdamW(algo.parameters(), lr=lr, betas=(beta1, beta2), weight_decay=weight_decay)
 
+for name, param in algo.named_parameters():
+    print(f"Algo Parameter to optimize {name} with shape {param.shape}")
+
+local_step = 0
+
 while env_step < total_steps:
 
     if termination_signal:
         #  Load the terminal state, I dont think we need to do this
 
         # Reset the environment
-        observations, infos = env.reset()
+        observations, infos = env.reset(seed=0)
         termination_signal = False
         cumulative_rewards = 0
         rewards = None
-        # env_step = 0
+        local_step = 0
         
 
     actions, q_total = algo.forward(observations)  # Forward pass
+    actions = {agent: np.clip(actions[agent],-1.0,1.0) for agent in env.agents}  # Filter actions for active agents
     # print("Actions from the model:", actions)
     
     # actions = algo.act(observations, cumulative_rewards, env_step)  # Get actions from the DTCG algorithm
     # actions = {agent: env.action_space(agent).sample() for agent in env.agents} # this is a dictionary
     observations, rewards, terminations, truncations, infos = env.step(actions)
     env_step += 1
+    local_step += 1
 
     total_rewards = sum(rewards.values()) if rewards else 0 # place holder for total rewards 
     cumulative_rewards += total_rewards
 
     
-    loss = algo.loss(q_total, torch.tensor([cumulative_rewards], dtype=torch.float32))
-    print("Loss:", loss.item())
+    loss = algo.loss(q_total, torch.tensor([cumulative_rewards], dtype=torch.float32).unsqueeze(0))  # Compute loss
+    if local_step == 50:
+        print("Loss:", loss.item())
+    
     algo.train()
     loss.backward()
+    optimizer.step()
     optimizer.zero_grad()
     
 
--- adj_mask/qmix.py
+++ adj_mask/qmix.py
@@ -7,7 +7,6 @@ class QMix(nn.Module):
         self.state_space = sum([state_space[agent].shape[0] for agent in agents])
         self.hidden_dim = hidden_dim
         for i, agent in enumerate(agents):
-            # print(state_space[agent].shape[0], action_space[agent].shape[0])
             setattr(self, f"agent_{i}_policy_network", nn.Linear(state_space[agent].shape[0], action_space[agent].shape[0]))
             setattr(self, f"agent_{i}_q_network", nn.Linear(state_space[agent].shape[0] + action_space[agent].shape[0], 1))
         self.hyper_network_weight_1 = nn.Linear(self.state_space, hidden_dim*self.num_agents) # W1 weights
@@ -22,35 +21,24 @@ class QMix(nn.Module):
         for i in range(self.num_agents):
             agent_policy_network = getattr(self, f"agent_{i}_policy_network")
             agent_action = agent_policy_network(states[:, i, :])
-            # print("agent_action", agent_action.shape)
             actions.append(agent_action)
             agent_q_network = getattr(self, f"agent_{i}_q_network")
             agent_q_input = torch.cat([states[:, i, :], agent_action], dim=-1)
-            # print("agent_q_input", agent_q_input.shape)
             agent_q_value = agent_q_network(agent_q_input)
-            # print("agent_q_value", agent_q_value.shape)
             q_values.append(agent_q_value)
         actions = torch.stack(actions, dim=1)
-        # print("actions", actions.shape)
         q_values = torch.cat(q_values, dim=-1)
-        # print("q_values", q_values.shape)
 
 
         hyper_state = torch.cat([states[:, i, :] for i in range(self.num_agents)], dim=-1)
         hyper_w1 = torch.abs(self.hyper_network_weight_1(hyper_state).view(-1, self.num_agents, self.hidden_dim))  # (batch_size, self.num_agents, hidden_dim)
         hyper_b1 = self.hyper_network_bias_1(hyper_state).view(-1, 1, self.hidden_dim)  # (batch_size, 1, hidden_dim)
-        # print(q_values.unsqueeze(0).shape, hyper_w1.shape, hyper_b1.shape)
         hidden = torch.relu(torch.bmm(q_values.unsqueeze(0), hyper_w1) + hyper_b1)
         hyper_w2 = torch.abs(self.hyper_network_weight_2(hyper_state).view(-1, self.hidden_dim, 1))  # (batch_size, hidden_dim, 1)
         hyper_b2_prep = torch.relu(self.hyper_network_bias_2_1(hyper_state).view(-1, self.hidden_dim))  # (batch_size, hidden dim)
         hyper_b2 = self.hyper_network_bias_2_2(hyper_b2_prep).view(-1, 1)  # (batch_size, 1, 1)
-        # print("hidden", hidden.shape, hyper_w2.shape, hyper_b2.shape)
         q_total = torch.bmm(hidden, hyper_w2) + hyper_b2  # (batch_size,  1)
-        # print("q_total", q_total.shape)
         q_total = q_total.squeeze(-1) # (batch_size,)
-        # print("q_total_after_squeeze", q_total.shape)
-
-
 
         return actions, q_total
     
