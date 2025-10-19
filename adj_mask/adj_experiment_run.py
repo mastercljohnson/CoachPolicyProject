c76952f03a9b54914a827aee5cc6287dadcfec4f
Christopher Johnson <mastercljohnson@gmail.com>
2025-10-19 17:41:14 -0400

    even more cleanup

--- adj_mask/adj_experiment_run.py
+++ adj_mask/adj_experiment_run.py
@@ -4,14 +4,16 @@ import torch
 import numpy as np
 import math
 
-# from torch.utils.tensorboard import SummaryWriter
+#  Boiler plate code for tensorboard logging, keeping here for intended future usage
 
+# from torch.utils.tensorboard import SummaryWriter
 # writer = SummaryWriter(log_dir="./runs/exp1")
+#  Log to TensorBoard when an episode ends
+# writer.add_scalar("Scaled Reward per timestep/episode", cumulative_rewards/local_step, episode)
+# writer.add_scalar("Loss/episode", q_acc, episode)
 
 # observation is a dictionary with keys as agent names and values as their respective observations
 # action is a dictionary with keys as agent names and values as their respective actions
-# Not sure what infos is
-
 env = multiwalker_v9.parallel_env(render_mode="human",terminate_on_fall=True)
 observations, infos = env.reset()
 
@@ -21,9 +23,7 @@ algo = AdjFrame(3, env.agents,60,60, env.observation_spaces, env.action_spaces)
 
 algo.rollout(300, env)
 
-#  Log to TensorBoard when an episode ends
-# writer.add_scalar("Scaled Reward per timestep/episode", cumulative_rewards/local_step, episode)
-# writer.add_scalar("Loss/episode", q_acc, episode)
+
     
 
 env.close()
