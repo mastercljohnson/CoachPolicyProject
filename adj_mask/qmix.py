0dade259f2983f583acdc529e98ca5f1aacded55
Christopher Johnson <mastercljohnson@gmail.com>
2025-10-19 17:31:19 -0400

    Remove unnecessary forward function

--- adj_mask/qmix.py
+++ adj_mask/qmix.py
@@ -19,36 +19,7 @@ class QMix(nn.Module):
         self.hyper_network_bias_1 = nn.Linear(self.hidden_dim*self.num_agents, hidden_dim) # bias
         self.hyper_network_weight_2 = nn.Linear(self.hidden_dim*self.num_agents, hidden_dim) # W2 weights
         self.hyper_network_bias_2_1 = nn.Linear(self.hidden_dim*self.num_agents, hidden_dim) # bias2
-
         self.hyper_network_bias_2_2 = nn.Linear(hidden_dim, 1) # bias2
-
-        # self.hyper_network_optimizer = torch.optim.AdamW(list(self.hyper_network_weight_1.parameters()) + list(self.hyper_network_bias_1.parameters()) + list(self.hyper_network_weight_2.parameters()) + list(self.hyper_network_bias_2_1.parameters()) + list(self.hyper_network_bias_2_2.parameters()), lr=learning_rate, betas=(beta1, beta2), weight_decay=weight_decay)
-    
-    def forward(self, states):
-        actions = []
-        q_values = []
-        for i in range(self.num_agents):
-            agent_policy_network = getattr(self, f"agent_{i}_policy_network")
-            agent_action, log_prob = self.get_action(states[:, i, :], i)
-            actions.append(agent_action)
-            agent_critic_network = getattr(self, f"agent_{i}_critic_network")
-            agent_q_input = states[:, i, :]
-            agent_q_value = agent_critic_network(agent_q_input)
-            q_values.append(agent_q_value)
-        q_values = torch.cat(q_values, dim=-1)
-
-
-        hyper_state = torch.cat([states[:, i, :] for i in range(self.num_agents)], dim=-1)
-        hyper_w1 = torch.abs(self.hyper_network_weight_1(hyper_state).view(-1, self.num_agents, self.hidden_dim))  # (batch_size, self.num_agents, hidden_dim)
-        hyper_b1 = self.hyper_network_bias_1(hyper_state).view(-1, 1, self.hidden_dim)  # (batch_size, 1, hidden_dim)
-        hidden = torch.relu(torch.bmm(q_values.unsqueeze(0), hyper_w1) + hyper_b1)
-        hyper_w2 = torch.abs(self.hyper_network_weight_2(hyper_state).view(-1, self.hidden_dim, 1))  # (batch_size, hidden_dim, 1)
-        hyper_b2_prep = torch.relu(self.hyper_network_bias_2_1(hyper_state).view(-1, self.hidden_dim))  # (batch_size, hidden dim)
-        hyper_b2 = self.hyper_network_bias_2_2(hyper_b2_prep).view(-1, 1)  # (batch_size, 1, 1)
-        q_total = torch.bmm(hidden, hyper_w2) + hyper_b2  # (batch_size,  1)
-        q_total = q_total.squeeze(-1) # (batch_size,)
-
-        return actions, q_total
     
     def act(self, states):
         actions = []
